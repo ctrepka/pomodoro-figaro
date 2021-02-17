b57b223f07073d350565a51e9d9d5d4fdc8cde23
Chris Repka <chris.repka@twdb.texas.gov>
2021-02-16 20:24:02 -0600

    getting started
    getting started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Col, Input, InputNumber, Layout, Row } from "antd";
+import { SprintTimer } from "./components/sprintTimer";
+import YouTube from "react-youtube";
+import { useState } from "react";
+
+const { Header, Footer, Content } = Layout;
+
+
+const timerConfig = {
+  sprintDuration: 60,
+  sprintsPerCycle: 4,
+  sprintBreakDuration: 10,
+  longBreakDuration: 30,
+  cycles: 4,
+}
 
 function App() {
+  const [player, setPlayer] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout
+      style={{
+        display: "grid",
+        gridTemplateRows: "auto 1fr auto",
+        height: "100vh",
+      }}
+    >
+      <Header theme="light">Header</Header>
+      <Content style={{ padding: "1rem" }}>
+        <Row style={{ height: "100%" }}>
+          <Col
+            xs={{ span: 24 }}
+            md={{ span: 8 }}
+            lg={{ span: 6 }}
+            style={{
+              padding: "1rem",
+              border: "1px solid #999",
+              borderRadius: ".25rem",
+              height: "100%",
+            }}
+          >
+            <Input.Group>
+              <h1>Pomodoro Config</h1>
+              <Input.Group compact style={{ padding: "0.5rem 0rem" }}>
+                <Input placeholder="Task name" style={{ width: "60%" }} />
+                <InputNumber
+                  defaultValue={1}
+                  placeholder="# Long cycles"
+                  style={{ width: "40%" }}
+                />
+              </Input.Group>
+              <h3>Sprint details</h3>
+              <Input.Group compact>
+                <InputNumber
+                  defaultValue={60 * 25}
+                  placeholder="Sprint duration"
+                  style={{ width: "50%" }}
+                />
+                <InputNumber
+                  defaultValue={60 * 5}
+                  placeholder="Sprint break duration"
+                  style={{ width: "50%" }}
+                />
+              </Input.Group>
+              <h3>Long cycle details</h3>
+              <Input.Group compact style={{ padding: "0.5rem 0rem" }}>
+                <InputNumber
+                  defaultValue={4}
+                  placeholder="Sprints in long cycle"
+                  style={{ width: "50%" }}
+                />
+                <InputNumber
+                  defaultValue={60 * 15 /*seconds * minutes*/}
+                  placeholder="Long cycle break duration"
+                  style={{ width: "50%" }}
+                />
+              </Input.Group>
+            </Input.Group>
+            <br />
+            <Button type="primary">Start</Button>
+          </Col>
+          <Col
+            xs={{ span: 24 }}
+            md={{ span: 16 }}
+            lg={{ span: 18 }}
+            style={{ padding: "1rem" }}
+          >
+            <YouTube videoId={"2lLBLjn3FCI"} opts={{origin: "http://localhost:3000", autoplay: 1}} onReady={setPlayer} />
+            <SprintTimer
+              player={player}
+              {...timerConfig} 
+            />
+          </Col>
+        </Row>
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
   );
 }
 
---
+++ src/components/sprintTimer.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+
+export function SprintTimer({
+  sprintDuration,
+  sprintsPerCycle,
+  sprintBreakDuration,
+  longBreakDuration,
+  cycles,
+  player,
+}) {
+  const [isBreak, setIsBreak] = useState(false);
+  const [play, setPlay] = useState(true);
+  const [sprintsRemaining, setSprintsRemaining] = useState(
+    sprintsPerCycle * cycles
+  );
+  const [time, setTime] = useState(sprintDuration);
+
+  useEffect(() => {
+    //if play is true and time remaining is > 0, remove 1 second
+    if (play && time > 0) {
+      const timer = setTimeout(() => {
+        setTime((time) => time - 1);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [setTime, play, time]);
+
+  useEffect(() => {
+    //if time is 0 && isBreak === false, set isBreak to true and setTime to sprintBreakDuration
+    if (time === 0 && !isBreak) {
+      setIsBreak(true);
+      setTime(sprintBreakDuration);
+      setSprintsRemaining((sprintsRemaining) => sprintsRemaining - 1);
+      //beep();
+    }
+
+    //if time is 0 && isBreak === true && sprintsRemaining > 0, setTime to sprintDuration and set isBreak to False
+    if (time === 0 && isBreak && sprintsRemaining > 0) {
+      setIsBreak(false);
+      setTime(sprintDuration);
+      //beep();
+    }
+  }, [
+    isBreak,
+    setIsBreak,
+    setTime,
+    time,
+    sprintBreakDuration,
+    sprintsRemaining,
+    setSprintsRemaining,
+    sprintDuration,
+  ]);
+
+  useEffect(() => {
+    if (isBreak && player.target.pauseVideo) {
+        player.target.pauseVideo()
+    } else {
+        if(player && player.target.playVideo){
+            player.target.playVideo()
+        }
+    }
+  }, [isBreak, player]);
+
+  return (
+    <div>
+      <h1 style={{ color: isBreak ? "green" : "orange" }}>
+        {isBreak ? "Break Time! Take a rest, pal!" : "Get working, punk."}
+      </h1>
+      <h2>{time}</h2>
+      <button onClick={() => setPlay((play) => !play)}>
+        {play ? "pause" : "resume"}
+      </button>
+      <p>
+        {Math.floor(
+          (sprintsPerCycle * cycles - sprintsRemaining) / sprintsPerCycle
+        )}
+        /{cycles} cycles completed | sprints left this cycle:{" "}
+        {sprintsRemaining % cycles} | total sprints remaining:{" "}
+        {sprintsRemaining}
+      </p>
+
+    </div>
+  );
+}
